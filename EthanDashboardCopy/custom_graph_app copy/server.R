d27911a6623fed4fc3adc5ded5899ce9c2cbe2b5
eddandomcho <minwoongethancho@gmail.com>
2025-06-06 15:47:32 -0400

    custom graph app + stuff

--- EthanDashboardCopy/custom_graph_app copy/server.R
+++ EthanDashboardCopy/custom_graph_app copy/server.R
@@ -1,13 +1,14 @@
 server <- function(input, output, session) {
+  # reactiveVals
   
+  # Observe for whether a country is chosen using the dropdown menu or
+  # chosen directly from the map.
   
   chosen_country <- reactiveVal(NULL)
-  
   observeEvent(input$country_select, {
     click<-input$country_select
     chosen_country(click)
   })
-  
   observeEvent(input$map_shape_click, {
     click <- input$map_shape_click
     chosen_country(click$id) 
@@ -22,6 +23,13 @@ server <- function(input, output, session) {
     )
   })
   
+  
+  pal_reactive <- reactive({
+      colorNumeric("YlGnBu", world_joined$population, na.color = "transparent")
+    })
+  
+  # -----------------------------------------------------------------------------
+  
   country_dataset <- reactiveVal(NULL)
   
   observeEvent(input$country_select, {
@@ -30,19 +38,156 @@ server <- function(input, output, session) {
     country_dataset(dataset)
   })
   
-  output$countryDisplay <- renderText({
-    if (is.null(chosen_country())) {
-      paste("Choose a country")
-    } else {
-      paste0("You chose ",chosen_country(), "!")
+  # -----------------------------------------------------------------------------
+  
+  # renderUI outputs. These are used to change the UI based on whether "Global"
+  # or "Country" is chosen.
+  
+  output$global_or_country_components <- renderUI ({
+    
+    
+    # GLOBAL COMPARISON
+    
+    
+    if (input$global_or_country == "global") {
+      tagList(
+        
+        # DISPLAY BIVARIATE SCATTER PLOTS
+        
+        tags$h4("Bivariate Graphs!",
+                style = "color: #003087; text-align: center;
+              font-style: italic"),
+        
+        
+        
+        # CHOOSE FIRST INDICATOR (GLOBAL)
+        
+        selectInput("first_indicator_global", 
+                    "Choose your first indicator",
+                    choices = global_level_choices,
+                    selected = "Gov_effect.sc"
+                    
+        ),
+        
+        
+        
+        # CHOOSE SECOND INDICATOR (GLOBAL)
+        
+        selectInput("second_indicator_global", 
+                    "Choose your second indicator",
+                    choices = global_level_choices,
+                    selected = "le.ineq.log.sc"
+                    
+        ),
+        
+        
+        # DISPLAYS CUSTOM SCATTERPLOT (GLOBAL-LEVEL)
+        
+        plotOutput("global_custom_scatter"),
+        
+        # DISPLAYS PEARSON AND SPEARMAN CORRELATION COEFFICIENTS
+        
+        verbatimTextOutput("global_correlation")
+        
+      ) 
+
+      
+      # -----------------------------------------------------------------------------
+      
+      
+    # COUNTRY-LEVEL COMPARISON  
+      
+    } else if (input$global_or_country == "country") {
+      tagList(
+        
+        # SELECT COUNTRY
+        
+        selectInput("country_select", "Select a Country to Investigate",
+                    choices = sort(unique(df_population$COUNTRYNM)),
+                    selected = "Japan"),
+        
+        #DISPLAYS WHAT COUNTRY WAS CHOSEN
+        
+        textOutput("countryDisplay"),
+        
+        # DISPLAYS COUNTRY FLAG
+        
+        tags$div(
+          style = "text-align: center;",
+          imageOutput("country_flag", height = "120px")
+        ),
+        
+        # DISPLAY HISTOGRAMS HERE
+        
+        tags$h4("Histograms!",
+                style = "color: #003087; text-align: center;
+              font-style: italic"),
+        
+        selectInput("country_histogram_indicator", 
+                    "Choose an indicator",
+                    choices = c(
+                      "Distance to Coast (km)" = "distance_to_coast_km", 
+                      "Degraded Ecosystems" = "mean.count.grav.V2.log.sc",
+                      "Relative Deprivation Index" = "povmap.grdi.v1.sc",
+                      "Coastal Vulnerability" = "perc.pop.world.coastal.merit.10m.log.sc"
+                    ),
+                    selected = "povmap.grdi.v1.sc"),
+        
+        plotOutput("country_histogram"),
+        
+        
+        # DISPLAY BIVARIATE SCATTER PLOTS
+        
+        tags$h4("Bivariate Graphs!",
+                style = "color: #003087; text-align: center;
+              font-style: italic"),
+        
+        # CHOOSE FIRST INDICATOR (COUNTRY-LEVEL)
+        
+        selectInput("first_indicator", 
+                    "Choose your first indicator",
+                    choices = c(
+                      "Distance to Coast (km)" = "distance_to_coast_km", 
+                      "Degraded Ecosystems" = "mean.count.grav.V2.log.sc",
+                      "Relative Deprivation Index" = "povmap.grdi.v1.sc",
+                      "Coastal Vulnerability" = "perc.pop.world.coastal.merit.10m.log.sc"
+                    ),
+                    selected = "povmap.grdi.v1.sc"
+                    
+        ),
+        
+        # CHOOSE SECOND INDICATOR (COUNTRY-LEVEL)
+        
+        selectInput("second_indicator", 
+                    "Choose your second indicator",
+                    choices = c(
+                      "Distance to Coast (km)" = "distance_to_coast_km", 
+                      "Degraded Ecosystems" = "mean.count.grav.V2.log.sc",
+                      "Relative Deprivation Index" = "povmap.grdi.v1.sc",
+                      "Coastal Vulnerability" = "perc.pop.world.coastal.merit.10m.log.sc"
+                    ),
+                    selected = "perc.pop.world.coastal.merit.10m.log.sc"
+                    
+        ),
+        
+        # DISPLAYS CUSTOM SCATTERPLOT (COUNTRY-LEVEL)
+        
+        plotOutput("custom_scatter"),
+        
+        # DISPLAYS PEARSON AND SPEARMAN CORRELATION COEFFICIENTS
+        
+        verbatimTextOutput("correlation")
+        
+      )
+      
     }
   })
   
   
+  # -----------------------------------------------------------------------------
   
-  pal_reactive <- reactive({
-    colorNumeric("YlGnBu", world_joined$population, na.color = "transparent")
-  })
+  
+  # Output interactive map in terms of POPULATION
   
   output$map <- renderLeaflet({
     pal <- pal_reactive()
@@ -75,29 +220,11 @@ server <- function(input, output, session) {
     
   })
   
-  # Zooming in on the selected country
-  # 
-  # observeEvent(input$country_select, {
-  #   req(input$country_select)
-  #   
-  #   if (input$country_select %in% world_joined$name) {
-  #     selected_country <- dplyr::filter(world_joined, name == input$country_select)
-  #     bbox <- st_bbox(selected_country)
-  #     
-  #     leafletProxy("map") %>%
-  #       fitBounds(
-  #         lng1 = bbox["xmin"],
-  #         lat1 = bbox["ymin"],
-  #         lng2 = bbox["xmax"],
-  #         lat2 = bbox["ymax"]
-  #       )
-  #   }
-  # })
-  # 
-  # 
-  # CUSTOM BIVARIATE SCATTERPLOT
   
-
+  # -----------------------------------------------------------------------------
+  
+  
+  # CUSTOM BIVARIATE SCATTERPLOT
   
   output$custom_scatter <- renderPlot({
     data <- country_dataset() # set it here because country_dataset is a function
@@ -107,7 +234,7 @@ server <- function(input, output, session) {
     y_col <- input$second_indicator
     
     # reverse so that i can find the labels for each choice
-    
+  
     indicator_choices <- c(
       "Distance to Coast (km)" = "distance_to_coast_km", 
       "Degraded Ecosystems" = "mean.count.grav.V2.log.sc",
@@ -130,8 +257,42 @@ server <- function(input, output, session) {
          main = chosen_country(),
          xlab = x_label,
          ylab = y_label)
-  
+    
   })
+  
+  
+  output$global_custom_scatter <- renderPlot({
+    data <- average_country_nogeo # set it here because country_dataset is a function
+    if (is.null(data)) return() 
+    
+    x_col <- input$first_indicator_global
+    y_col <- input$second_indicator_global
+    
+    # reverse so that i can find the labels for each choice
+    
+    indicator_choices <- global_level_choices
+    
+    x_label <- names(indicator_choices)[indicator_choices == x_col]
+    y_label <- names(indicator_choices)[indicator_choices == y_col]
+    
+    if (!(x_col %in% names(data)) || !(y_col %in% names(data))) return()
+    
+    data1 <- data[[x_col]]
+    data2 <- data[[y_col]]
+    
+    # Check if the data vectors have at least some non-NA numeric values
+    if (all(is.na(data1)) || all(is.na(data2))) return()
+    
+    plot(data1, data2, 
+         main = "Global",
+         xlab = x_label,
+         ylab = y_label)
+    
+  })
+  
+  # -----------------------------------------------------------------------------
+  
+  # Outputs the Pearson and Spearman Coefficients
   
   output$correlation <- renderText({
     data <- country_dataset() # set it here because country_dataset is a function
@@ -143,8 +304,8 @@ server <- function(input, output, session) {
     cor = cor(data[[x_col]], data[[y_col]])
     
     cor = round(cor, 4)
-  
-
+    
+    
     spr_cor = cor(data[[x_col]], data[[y_col]], method = "spearman")
     spr_cor = round(spr_cor, 4)
     
@@ -152,7 +313,64 @@ server <- function(input, output, session) {
           "\nSpearman Coefficient (rho) = ", spr_cor)
     
   })
-
+  
+  
+  output$global_correlation <- renderText({
+    data <- average_country_nogeo
+    if (is.null(data)) return() 
+    
+    x_col <- input$first_indicator_global
+    y_col <- input$second_indicator_global
+    
+    cor = cor(data[[x_col]], data[[y_col]])
+    
+    cor = round(cor, 4)
+    
+    
+    spr_cor = cor(data[[x_col]], data[[y_col]], method = "spearman")
+    spr_cor = round(spr_cor, 4)
+    
+    paste("Pearson Coefficient (r) = ", cor,
+          "\nSpearman Coefficient (rho) = ", spr_cor)
+    
+  })
+  
+  # -----------------------------------------------------------------------------
+  
+  # Displays a histogram for a COUNTRY
+  
+  output$country_histogram <- renderPlot({
+    
+    chi <- input$country_histogram_indicator
+    
+    data <- country_dataset() # set it here because country_dataset is a function
+    if (is.null(data)) return() 
+    
+    col = data[[chi]]
+    
+    indicator_choices <- c(
+      "Distance to Coast (km)" = "distance_to_coast_km", 
+      "Degraded Ecosystems" = "mean.count.grav.V2.log.sc",
+      "Relative Deprivation Index" = "povmap.grdi.v1.sc",
+      "Coastal Vulnerability" = "perc.pop.world.coastal.merit.10m.log.sc"
+    )
+    
+    label <- names(indicator_choices)[indicator_choices == chi]
+    
+    countryname <- chosen_country()
+    
+    hist(col,
+         main = paste0("Histogram of ", label, " for ", countryname),
+         xlab = label)
+    
+  })
+  
+  
+  
+  # -----------------------------------------------------------------------------
+  
+  # Displays the country flag image
+  
   output$country_flag <- renderImage({
     # before click
     if (is.null(chosen_country())) {
@@ -175,4 +393,9 @@ server <- function(input, output, session) {
     }
   }, deleteFile = FALSE)
   
+  
 }
+
+# NOTE: Could have the side bar show one MAIN indicator they want to see that is displayed on the
+# map, and that displays the histogram. Below that we could have them choose another indicator they'd
+# like it to compare to
--- EthanDashboardCopy/custom_graph_app copy/ui.R
+++ EthanDashboardCopy/custom_graph_app copy/ui.R
@@ -1,65 +1,39 @@
 ui <- page_sidebar(
+    input_dark_mode(id = "mode"), 
     sidebar = sidebar(
-      width = 400,
+      width = 450,
       tags$h1("Make Your Own CUSTOM GRAPH!",
               style = "color: #003087;
             font-weight: bold"),
-      selectInput("country_select", "Select a Country to Compare",
-                  choices = sort(unique(df_population$COUNTRYNM)),
-                  selected = "Japan"),
       
-      textOutput("countryDisplay"),
+      "It's important to be able to visualize data, and we wanted to make this data easily 
+      accessible for you! Select TWO INDICATORS and see how these indicators compare across
+      countries and within countries",
       
-      tags$div(
-        style = "text-align: center;",
-        imageOutput("country_flag", height = "120px")
-      ),
+      tags$h6("Global: Each data point represents the average score of a country." ,
+              style = "font-style: italic"),
       
-      # selectInput("score_type", "Select Indicator to Display:",
-      #             choices = c(
-      #               "Governance Quality" = "governance_score",
-      #               "Social Inequality" = "inequality_score",
-      #               "Economic Dependence" = "economic_dependence_score",
-      #               "Coastal Exposure" = "coastal_exposure_score"
-      #             ),
-      #             selected = "inequality_score"
-      # ),
       
-      # "Do you want to create your own graph? Here you can make it for yourself!",
+      tags$h6("Country: Each data point represents the score of a district within a chosen country." ,
+              style = "font-style: italic"),
       
-      tags$h4("Bivariate Graphs!",
-              style = "color: #003087;
-            font-style: italic"),
       
-      selectInput("first_indicator", 
-                  "Choose your first indicator",
-                  choices = c(
-                    "Distance to Coast (km)" = "distance_to_coast_km", 
-                    "Degraded Ecosystems" = "mean.count.grav.V2.log.sc",
-                    "Relative Deprivation Index" = "povmap.grdi.v1.sc",
-                    "Coastal Vulnerability" = "perc.pop.world.coastal.merit.10m.log.sc"
-                  ),
-                  selected = "povmap.grdi.v1.sc"
+      # GLOBAL OR COUNTRY-LEVEL
       
-                ),
+      selectInput("global_or_country", "Select level of investigation:",
+                  choices = c("Global" = "global",
+                              "Country" = "country"),
+                  selected = "Global"),
       
-      selectInput("second_indicator", 
-                  "Choose your second indicator",
-                  choices = c(
-                    "Distance to Coast (km)" = "distance_to_coast_km", 
-                    "Degraded Ecosystems" = "mean.count.grav.V2.log.sc",
-                    "Relative Deprivation Index" = "povmap.grdi.v1.sc",
-                    "Coastal Vulnerability" = "perc.pop.world.coastal.merit.10m.log.sc"
-                  ),
-                  selected = "perc.pop.world.coastal.merit.10m.log.sc"
-                  
-      ),
       
-      plotOutput("custom_scatter"),
+      # REACTIVE FOR WHETHER GLOBAL OR COUNTRY IS SELECTED
       
-      verbatimTextOutput("correlation")
+      uiOutput("global_or_country_components")
       
   ),
+  
+  # DISPLAYS WORLD MAP
+  
   leafletOutput("map", width = "90%", height = 200)
   
   
