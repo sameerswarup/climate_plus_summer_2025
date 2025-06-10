58696586ffb8e0c64ded01d65c125e57d4d73e98
Abhinav Meduri <avm29@duke.edu>
2025-06-10 14:37:43 -0400

    Dashboard updates (zoom not working)

--- AbhinavEDA/shiny_app/ui.R
+++ AbhinavEDA/shiny_app/ui.R
@@ -7,12 +7,13 @@ ui <- page_sidebar(
   sidebar = sidebar(
     width = 400,
     tags$h2("Inequity Indicators Map"),
-    selectInput("indicator_category", "Choose Indicator Category:", 
+    selectInput("indicator_category", "Choose Indicator Category:",
                 choices = indicator_choices, selected = "Governance"),
-    selectInput("mean_type", "Choose Mean Type:", 
-                choices = mean_choices, selected = "Arithmetic Mean")
+    selectInput("mean_type", "Choose Mean Type:",
+                choices = mean_choices, selected = "Arithmetic Mean"),
+    selectizeInput("country_search", "Jump to Country:", choices = NULL, selected = NULL),
+    actionButton("zoom_button", "Zoom to Selected Country")
   ),
   leafletOutput("map", height = 600)
 )
-
 
