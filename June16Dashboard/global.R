69560145fa774c89c9d4b5c9143a554fa4767817
Leonard Eshun <leonardeshun@gmail.com>
2025-07-07 15:41:10 -0400

    Updated tif file paths for merged global file

--- June16Dashboard/global.R
+++ June16Dashboard/global.R
@@ -136,56 +136,56 @@ climate_data_options <- list(
   
   "Coral Bleaching Heat" = list(
     "Degree Heating Weeks" = list(
-      "1-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250401.tif",
-      "2-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250402.tif",
-      "3-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250403.tif",
-      "4-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250404.tif",
-      "5-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250405.tif",
-      "6-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250406.tif",
-      "7-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250407.tif",
-      "8-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250408.tif",
-      "9-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250409.tif",
-      "10-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250410.tif",
-      "11-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250411.tif",
-      "12-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250412.tif",
-      "13-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250413.tif",
-      "14-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250414.tif",
-      "15-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250415.tif",
-      "16-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250416.tif",
-      "17-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250417.tif",
-      "18-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250418.tif",
-      "19-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250419.tif",
-      "20-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250420.tif",
-      "21-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250421.tif",
-      "22-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250422.tif",
-      "23-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250423.tif",
-      "24-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250424.tif",
-      "25-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250425.tif",
-      "26-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250426.tif",
-      "27-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250427.tif",
-      "28-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250428.tif",
-      "29-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250429.tif",
-      "30-April-2025" = "../Coral_Bleaching_data/ct5km_dhw_v3.1_20250430.tif"
+      "1-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250401.tif",
+      "2-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250402.tif",
+      "3-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250403.tif",
+      "4-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250404.tif",
+      "5-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250405.tif",
+      "6-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250406.tif",
+      "7-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250407.tif",
+      "8-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250408.tif",
+      "9-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250409.tif",
+      "10-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250410.tif",
+      "11-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250411.tif",
+      "12-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250412.tif",
+      "13-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250413.tif",
+      "14-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250414.tif",
+      "15-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250415.tif",
+      "16-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250416.tif",
+      "17-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250417.tif",
+      "18-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250418.tif",
+      "19-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250419.tif",
+      "20-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250420.tif",
+      "21-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250421.tif",
+      "22-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250422.tif",
+      "23-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250423.tif",
+      "24-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250424.tif",
+      "25-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250425.tif",
+      "26-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250426.tif",
+      "27-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250427.tif",
+      "28-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250428.tif",
+      "29-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250429.tif",
+      "30-April-2025" = "data/Coral_Bleaching_data/ct5km_dhw_v3.1_20250430.tif"
     )
   ),
   "Sea Level Rise" = list(
     "Change (meters)" = list(
-      "Near Term (2021–2040)" = "../IPCC_data/SLR_near_term.tif",
-      "Medium Term (2041–2060)" = "../IPCC_data/SLR_medium_term.tif",
-      "Long Term (2081–2100)" = "../IPCC_data/SLR_long_term.tif"
+      "Near Term (2021–2040)" = "data/IPCC_data/SLR_near_term.tif",
+      "Medium Term (2041–2060)" = "data/IPCC_data/SLR_medium_term.tif",
+      "Long Term (2081–2100)" = "data/IPCC_data/SLR_long_term.tif"
     )
   ),
   
   "Heating Degree Days" = list(
     "Change" = list(
-      "Near Term (2021–2040)" = "../IPCC_data/DH_Near_Term_Change.tif",
-      "Medium Term (2041–2060)" = "../IPCC_data/DH_Medium_Term_Change.tif",
-      "Long Term (2081–2100)" = "../IPCC_data/DH_Long_Term_Change.tif"
+      "Near Term (2021–2040)" = "data/IPCC_data/DH_Near_Term_Change.tif",
+      "Medium Term (2041–2060)" = "data/IPCC_data/DH_Medium_Term_Change.tif",
+      "Long Term (2081–2100)" = "data/IPCC_data/DH_Long_Term_Change.tif"
     ),
     "Value" = list(
-      "Near Term (2021–2040)" = "../IPCC_data/DH_Near_Term_Value.tif",
-      "Medium Term (2041–2060)" = "../IPCC_data/DH_Medium_Term_Value.tif",
-      "Long Term (2081–2100)" = "../IPCC_data/DH_Long_Term_Value.tif"
+      "Near Term (2021–2040)" = "data/IPCC_data/DH_Near_Term_Value.tif",
+      "Medium Term (2041–2060)" = "data/IPCC_data/DH_Medium_Term_Value.tif",
+      "Long Term (2081–2100)" = "data/IPCC_data/DH_Long_Term_Value.tif"
     )
   )
 )
