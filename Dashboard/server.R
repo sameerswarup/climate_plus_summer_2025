6b308f09825b196995dd7f81887048423fb0e4f4
eddandomcho <minwoongethancho@gmail.com>
2025-07-23 11:29:59 -0400

    more cleaning of git repo

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -8,8 +8,7 @@ server <- function(input, output, session) {
   source("modules/climate_risk_map_2.R", local = TRUE)
   source("modules/ndGain_map_2.R", local = TRUE)
   source("modules/countryComparison.R", local = TRUE)
-  source("modules/dataOverview.R", local = TRUE)
-  
+
   output$map1 <- renderLeaflet({
     leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
       addProviderTiles(providers$Esri.WorldImagery) %>%
